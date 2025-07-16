60ac9b501334e87d105ee3bc07fb2b3fc61c0562
Hassen Muhammed <120062084+hassen396@users.noreply.github.com>
2025-07-16 12:50:54 +0000

    Configure GitHub Actions for Vite deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // <-- IMPORTANT: Use your repository name here
 })
+
